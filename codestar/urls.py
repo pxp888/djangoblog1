b54e79133de55c56c1805b6d0636f37215ecb062
pxp888 <pxperrine@gmail.com>
2024-01-26 00:25:31 +0100

    add summernote to admin

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
-# from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('blog/', blog_views.bloghome, name='blog'),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
 ]
 
