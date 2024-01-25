9cc5087215485d171a27d0a2ac4cb964adc6381b
pxp888 <pxperrine@gmail.com>
2024-01-25 09:26:08 +0100

    add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -93,6 +93,12 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
