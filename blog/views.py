60ae3e8d1b835214a07d2bd1af10228fc53e7b49
pxp888 <pxperrine@gmail.com>
2024-01-29 13:38:04 +0100

    add allauth

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
+
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "blog/index.html"
     paginate_by = 9
-
-
 
 
 def post_detail(request, slug):
